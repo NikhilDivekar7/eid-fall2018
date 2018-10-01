d7728ef0ca6e7493e44e9eeafd1a6d9dee778817
NikhilDivekar7 <31787984+NikhilDivekar7@users.noreply.github.com>
2018-09-30 23:33:25 -0600

    Add files via upload

---
+++ Project1/database.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+def createTable():
+    connection = sqlite3.connect("login.db")
+    connection.execute("CREATE TABLE USERS (USERNAME TEXT NOT NULL, EMAIL TEXT, PASSWORD TEXT)")
+    connection.execute("INSERT INTO USERS VALUES(?,?,?)",('nikhil','nd@gmail.com','abcd1234'))
+    connection.commit()
+    result = connection.execute("SELECT * FROM USERS")
+    for data in result:
+        print("Username: ", data[0])
+        print("Email: ", data[1])
+        print("Password: ", data[2])
+    connection.close()
+    
+createTable()
